2ee5e0a95925d8fef021db2a1b238c2798e9ef00
lutfisobri <lutfisobri@gmail.com>
2022-11-17 03:20:54 +0700

    add config

--- app/core/Database.php
+++ app/core/Database.php
@@ -11,22 +11,22 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Container\Container;
+$ini = parse_ini_file('config/php.ini');
 
 $database = new Capsule;
 $database->addConnection([
-    'driver' => 'mysql',
-    'host' => '127.0.0.1',
-    'port' => 3306,
-    'username' => 'root',
-    'password' => '',
-    'database' => 'new_presensi',
-    'charset' => 'utf8',
-    'collation' => 'utf8_unicode_ci',   
+    'driver' => $ini['db_driver'],
+    'host' => $ini['db_host'],
+    'port' => $ini['db_port'],
+    'username' => $ini['db_username'],
+    'password' => $ini['db_password'],
+    'database' => $ini['db_name'],
+    'charset' => $ini['db_charset'],
+    'collation' => $ini['db_collation'],   
 ]);
 
 
 $database->setEventDispatcher(new Dispatcher(new Container));
 
-// Make this Capsule instance available globally via static methods... (optional)
 $database->setAsGlobal();
 $database->bootEloquent();
--- app/init.php
+++ app/init.php
@@ -1,9 +1,9 @@
 <?php
 
 require_once 'core/app.php';
+require_once 'config/config.php';
 require_once 'core/Database.php';
 require_once 'core/Message.php';
-require_once 'config/config.php';
 require_once($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
 require_once 'vendor/autoload.php';
 require_once 'controllers/controller.php';
